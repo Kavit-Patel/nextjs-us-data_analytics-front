608bc0699e5a3560b857a4ef6ceafbbd090b176e
Kavit Patel <kvpatel.er@gmail.com>
2025-01-08 11:36:03 +0530

    minor change

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -2,12 +2,14 @@
 
 import { useUserLogin } from "@/lib/query";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import Loader from "./Loader";
 
 const GoogleLoginButton = () => {
   const { data: user } = useUserLogin();
   const router = useRouter();
+  const [isRedirecting, setIsRedirecting] = useState<boolean>(false);
   const googleAuthUrl = process.env.NEXT_PUBLIC_GOOGLE_AUTH_URL;
 
   if (!googleAuthUrl) {
@@ -19,14 +21,23 @@ const GoogleLoginButton = () => {
       router.push("/");
     }
   }, [router, user]);
+  const handleRedirect = () => {
+    setIsRedirecting(true);
+    window.location.href = googleAuthUrl || "#";
+  };
   return (
-    <a
-      href={googleAuthUrl || "#"}
+    <button
+      disabled={isRedirecting}
+      onClick={handleRedirect}
       className="w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 flex justify-center items-center"
       aria-label="Login with Google"
     >
-      Login with Google
-    </a>
+      {isRedirecting ? (
+        <Loader width={20} height={20} color="orange" />
+      ) : (
+        "Login with Google"
+      )}
+    </button>
   );
 };
 
